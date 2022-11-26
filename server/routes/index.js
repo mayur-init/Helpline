f2429b9389d592c1c938b25cf0b3a52fe2139778
mayur-init1 <mayurcosmos007@gmail.com>
2022-11-26 21:42:14 +0530

    routes updated

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const googleMapApiController = require('../contorllers/googleMapApiController');
 
-router.get('/', (req, res) =>{res.send('<h2>Project-Helpline: server is running...</h2>')});
+// router.get('/', (req, res) =>{res.send('<h2>Project-Helpline: server is running...</h2>')});
 
 //google map api releated routes
 router.get('/getNearby', googleMapApiController.getNearby);
