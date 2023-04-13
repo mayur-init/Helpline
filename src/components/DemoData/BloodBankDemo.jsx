c246655614ad9a9138a450816981479047264a82
Ankit Burman <ank2911it@gmail.com>
2023-04-13 15:34:17 +0530

    Blood Bank Services Added

---
+++ src/components/DemoData/BloodBankDemo.jsx
@@ -0,0 +1,58 @@
+const BloodBankDemo = [
+    {
+        id : "1",
+        name: "City Blood Bank",
+        address: "Bastin Bazar G.T Road Asansol-713302",
+        location:"Asansol",
+        number: "9738783109",
+        email: "citybank@gmail.com",
+        available:" AB+:4, AB-:1, B:36, O+:68",
+        category: "Govt."
+
+    },
+    {
+        id : "2",
+        name: "Tata Blood Center",
+        address: "S.B Gorai Road Asansol-713304",
+        location:"Asansol",
+        number: "7363874919",
+        email: "blood.tatagroup@gmail.com",
+        available:"O+:4,A+:10,o-:1",
+        category: "Private"
+
+    },
+    {
+        id : "3",
+        name: "Nihilent Blood Bank",
+        address: "Murgasol Opp. PNB Bank Asansol-713404",
+        location:"Asansol",
+        number: "9734332901",
+        email: "bloodseva@gmail.com",
+        available:"B-:4,AB-:6,o-:4,A+:9",
+        category: "Private"
+
+    },
+    {
+        id : "4",
+        name: "Virtusa Blood Bank",
+        address: "Rangania Para Asansol-823261",
+        location:"Asansol",
+        number: "6775547611",
+        email: "virbloodbank@gmail.com",
+        available:"Whole Blood",
+        category: "Govt."
+
+    },
+    {
+        id : "1",
+        name: "Asansol Dist. Hospital",
+        address: "Mohishila Colony Asansol-713304",
+        location:"Asansol",
+        number: "9002416561",
+        email: "disthospitalasansol@gmail.com",
+        available:"Whole Blood",
+        category: "Govt."
+
+    }
+];
+export default BloodBankDemo;
--- src/components/Heroes/BloodBankHero.jsx
+++ src/components/Heroes/BloodBankHero.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import BloodBankDemo from '../DemoData/BloodBankDemo';
 
 function BloodBankHero() {
-  return (
-    <div className='w-full h-[100vh] bg-white  flex flex-col justify-between'>
+    const [data, setData] = useState(BloodBankDemo);
+    return (
+        <div>
+            <div className='w-full h-[100vh] bg-white  flex flex-col justify-between'>
                 {/* Hero Page Start */}
                 <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
                     <div>
-                        <img className='w-full mix-blend-multiply' src={'assets/blood1.webp'} alt="/"/>
+                        <img className='w-full mix-blend-multiply' src={'assets/blood1.webp'} alt="/" />
                     </div>
                     <div className='flex flex-col justify-center md:items-start w-full px-12'>
                         <p className='text-3xl md:text-4xl font-semibold'>Donate <span className='text-red-600 font-bold'>Blood</span> Give The Gift of Life</p>
@@ -18,9 +21,36 @@ function BloodBankHero() {
                         </div>
                     </div>
                 </div>
-                {/* Hero Page Ends */}
+               
             </div>
-  )
+                 {/* Hero Page Ends */}
+
+                {/*BloodBank Details Start*/}
+
+                <h1 className='text-4xl font-bold my-10 text-violet-600 text-center hover:scale-110'>Our Blood Bank Details</h1>
+        
+                <div className='grid md:grid-cols-3 gap-3 mx-5'>
+                {
+                    data.map((item) => {
+                        const { id, name, address, location, number,email, available,category } = item;
+                        return (
+                            <div className="border min-h-[70vh] bg-white m-4 p-5 rounded-xl shadow-xl hover:scale-105" key={id}>
+                                    <h1 className="mx-2 font-bold text-3xl text-center text-violet-600 my-3">{name}</h1>
+                                    <h1 className='text-xl mt-7 font-semibold'>Address: {address}</h1>
+                                    <h2 className="text-xl my-3 font-semibold">City: {location}</h2>
+                                    <h2 className="text-xl my-3 font-semibold">Availability: <span className='text-red-600'>{available}</span></h2>
+                                    <h2 className="text-xl my-3 font-semibold">Category: {category}</h2>
+                                    <h2 className="text-xl my-3 font-semibold">Email: {email}</h2>
+                                    <h2 className="text-xl my-3 font-semibold">Contact No: {number}</h2>
+                            </div>
+                           
+                        )
+                    })
+                }
+                 </div>
+                {/*BloodBank Details Ends*/}
+        </div>
+    )
 }
 
 export default BloodBankHero
