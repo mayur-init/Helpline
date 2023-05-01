7c9f29f86827040e61513e5de28da7898f7245a0
Sahil-Soni1 <sonisahil334@gmail.com>
2023-05-01 23:27:58 +0530

    routes updated

--- server/controllers/hospital/hospitalBloodBankController.js
+++ server/controllers/hospital/hospitalBloodBankController.js
@@ -2,15 +2,27 @@ const {BloodBank} = require('../../models');
 
 exports.addBloodBank = async(req, res) => {
 
-    try{
-        const providerName = req.body.ServiceProviderName;
-        const email = req.body.Email;
-        const address = req.body.Address;
-        const regdId = req.body.RegdId;
-        const parentRegdId = req.body.ParentRegdId;
-        const contactNo = req.body.ContactNo;
-        const password = req.body.Password;
+    const providerName = req.body.ServiceProviderName;
+    const email = req.body.Email;
+    const address = req.body.Address;
+    const regdId = req.body.RegdId;
+    const parentRegdId = req.body.ParentRegdId;
+    const contactNo = req.body.ContactNo;
+    const password = req.body.Password;
 
+    if(!providerName || !email || !address || !contactNo || !password)
+        return res.status(422).json({error : "Some fields are empty"});
+
+    try{
+        const bloodBankExist = await BloodBank.findOne({contactNo : contactNo});
+        if(bloodBankExist){
+            return res.status(422).json({msg : "contact no already exist"});
+        }
+
+        const bloodBankExist1 = await BloodBank.findOne({email : email});
+        if(bloodBankExist1){
+            return res.status(422).json({msg : "email already exist"});
+        }
 
         const bloodBank = new BloodBank({
             providerName,
@@ -27,7 +39,7 @@ exports.addBloodBank = async(req, res) => {
     }
     catch(err){
         console.log(err);
-        res.status(400).json(err);
+        res.status(400).json({msg : "Some issue"});
     }
 };
 //done
@@ -36,7 +48,7 @@ exports.getBloodBanks = async(req, res) => {
         const hospitalId = req.params.regdId;
         const bloodBanks = await BloodBank.find({parentRegdId : hospitalId});
         if(bloodBanks.length === 0)
-            return res.status(404).json({msg : "Not found"});
+            return res.status(200).json({msg : "No data found"});
         res.status(200).json(bloodBanks);
     }
     catch(err){
@@ -46,10 +58,25 @@ exports.getBloodBanks = async(req, res) => {
 };
 //done
 exports.updateBloodBank = async(req, res) => {
+
+    const contactNo = req.body.contactNo;
+    const email = req.body.email;
     try{
+        if(contactNo){
+            const bloodBankExist = await BloodBank.findOne({contactNo : contactNo});
+            if(bloodBankExist){
+                return res.status(422).json({msg : "contact no already exist"});
+            }
+        }
+        if(email){
+            const bloodBankExist1 = await BloodBank.findOne({email : email});
+            if(bloodBankExist1){
+                return res.status(422).json({msg : "email already exist"});
+            }
+        }
         const bloodBankId = req.params.regdId;
-        const response = await BloodBank.update({"regdId" : bloodBankId}, {$set : req.body} , {new : true});
-        if(response === null)
+        const response = await BloodBank.findOneAndUpdate({"regdId" : bloodBankId}, {$set : req.body} , {new : true});
+        if(!response)
             return res.status(404).json({msg : "Not found"});
         res.status(200).json({msg : "Updated Successfully"});
     }catch(err){
@@ -60,8 +87,8 @@ exports.updateBloodBank = async(req, res) => {
 exports.deleteBloodBank = async(req, res) => {
     try{
         const bloodBankId = req.params.regdId;
-        const response = await BloodBank.remove({"regdId" : bloodBankId});
-        if(response === null)
+        const response = await BloodBank.findOneAndDelete({"regdId" : bloodBankId});
+        if(!response)
             return res.status(404).json({msg : "Not found"});
         res.status(200).json({msg : "Success"});
     }
--- server/controllers/oxygencylinder/oxygencylinderController.js
+++ server/controllers/oxygencylinder/oxygencylinderController.js
@@ -1,66 +1,95 @@
 const {OxygenCylinderProvider} = require('../../models');
 
-// to add a oxygencylinderprovider
 exports.addOxygenCylinderProvider = async(req, res) => {
 
+    const providerName = req.body.ServiceProviderName;
+    const email = req.body.Email;
+    const address = req.body.Address;
+    const regdId = req.body.RegdId;
+    const parentRegdId = req.body.ParentRegdId;
+    const contactNo = req.body.ContactNo;
+    const password = req.body.Password;
+
+    if(!providerName || !email || !address || !contactNo || !password)
+        return res.status(422).json({error : "Some fields are empty"});
+
     try{
-        const providerName = req.body.ServiceProviderName;
-        const email = req.body.Email;
-        const address = req.body.Address;
-        const regdId = req.body.RegdId;
-        const parentRegdId = req.body.ParentRegdId;
-        const contactNo = req.body.ContactNo;
-        const password = req.body.Password;
+            const oxygenCylinderExist = await OxygenCylinderProvider.findOne({contactNo : contactNo});
+            if(oxygenCylinderExist){
+                return res.status(422).json({msg : "contact no already exist"});
+            }
 
-        const oxygenCylinder = new OxygenCylinderProvider({
-            providerName,
-            email,
-            address,
-            regdId,
-            parentRegdId,
-            contactNo, 
-            password   
-        });
-        await oxygenCylinder.save();
+            const oxygenCylinderExist1 = await OxygenCylinderProvider.findOne({email : email});
+            if(oxygenCylinderExist1){
+                return res.status(422).json({msg : "email already exist"});
+            }
 
-        res.status(201).json({msg: "success"});
+            const oxygenCylinder = new OxygenCylinderProvider({
+                providerName,
+                email,
+                address,
+                regdId,
+                parentRegdId,
+                contactNo, 
+                password   
+            });
+            await oxygenCylinder.save();
+        
+            res.status(201).json({msg: "success"});
     }
     catch(err){
         console.log(err);
-        res.status(400).json(err);
+        res.status(400).json({msg : "Some issue"});
     }
 };
-
+//done
 exports.getOxygenCylinderProviders = async(req, res) => {
     try{
-        const oxygenCylinderProviders = await OxygenCylinderProvider.find();
-        if(oxygenCylinderProviders.length === 0)
-            return res.status(404).json({msg : "Not found"});
-        res.status(200).json(oxygenCylinderProviders);
+        const oxygenCylinders = await OxygenCylinderProvider.find();
+        if(oxygenCylinders.length === 0)
+            return res.status(404).json({msg : "No data exists"});
+        res.status(200).json(oxygenCylinders);
     }
     catch(err){
         console.log(err);
         res.status(400).json({msg : "Some issue"});
     }
 };
-
+//done
 exports.updateOxygenCylinder = async(req, res) => {
+    const contactNo = req.body.contactNo;
+    const email = req.body.email;
+
     try{
+        if(contactNo){
+            const oxygenCylinderExist = await OxygenCylinderProvider.findOne({contactNo : contactNo});
+            if(oxygenCylinderExist){
+                return res.status(422).json({msg : "contact no already exist"});
+            }
+        }
+        if(email){
+            const oxygenCylinderExist1 = await OxygenCylinderProvider.findOne({email : email});
+            if(oxygenCylinderExist1){
+                return res.status(422).json({msg : "email already exist"});
+            }
+        }
         const providerId = req.params.regdId;
-        const response = await OxygenCylinderProvider.updateOne({"regdId":providerId}, {$set : req.body} , {new : true});
-        if(response === null)
-            return res.status(404).json({msg : "Not found"});
-        res.status(200).json({msg : "Updated Successfully"});
+        const response = await OxygenCylinderProvider.findOneAndUpdate({"regdId" : providerId}, {$set : req.body} , {new : true});
+        if(response)
+            return res.status(200).json({msg : "Updated Successfully"});
+        res.status(404).json({msg : "Not found"});
+        
     }catch(err){
+        console.log(err);
         res.status(400).json({msg : "Some issue"});
     }
-}; 
-
+};
+//done
 exports.deleteOxygenCylinderProvider = async(req, res) => {
     try{
         const providerId = req.params.regdId;
-        const response = await OxygenCylinderProvider.remove({"regdId": providerId});
-        if(response === null)
+        const response = await OxygenCylinderProvider.findOneAndDelete({"regdId" : providerId});
+        if(!response)
             return res.status(404).json({msg : "Not found"});
         res.status(200).json({msg : "Success"});
     }
@@ -69,16 +98,17 @@ exports.deleteOxygenCylinderProvider = async(req, res) => {
         res.status(400).json({msg : "Some issue"});
     }
 };
-    
+//done
 exports.getParticularProvider = async(req, res) => {
     try{
         const oxyCyProviderId = req.params.regdId;
-        const oxygenCylinderProvider = await OxygenCylinderProvider.find({"regdId": oxyCyProviderId});
-        if(oxygenCylinderProvider === null)
+        const oxygenCylinderProvider = await OxygenCylinderProvider.find({"regdId" : oxyCyProviderId});
+        if(oxygenCylinderProvider.length === 0)
             return res.status(404).json({msg : "Not found"});
         res.status(200).json(oxygenCylinderProvider);
     }
     catch(err){
+        console.log(err);
         res.status(400).json({msg : "Some issue"});
     }
 }
