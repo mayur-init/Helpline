3d933e4de025df2268a0368342a2b56d4ed824bb
Ankit Burman <ank2911it@gmail.com>
2023-05-06 11:58:53 +0530

    no change

--- src/components/Loginforms/UserLogin.jsx
+++ src/components/Loginforms/UserLogin.jsx
@@ -32,7 +32,7 @@ function UserLogin({ }) {
         <p className='text-3xl font-bold text-gray-600 my-[4vh] flex justify-center hover:text-violet-600'>User Login</p>
         <div className='flex flex-col w-[50vh] h-auto mt-[8vh] mb-[4vh]'>
           <input type='text' placeholder='Name' className='border-2 border-gray-600 rounded-full px-4 py-1 my-2' onChange={(e) => { setUserName(e.target.value) }}></input>
-          <input type='text' placeholder='Contact' className='border-2 border-gray-600 rounded-full px-4 py-1 my-2' onChange={(e) => { setContactNo(e.target.value) }}></input>
+          <input type='number' placeholder='Contact' className='appearance-none border-2 border-gray-600 rounded-full px-4 py-1 my-2'  onWheel={(e) => e.target.blur()} onChange={(e) => { setContactNo(e.target.value) }}></input>
           <input type='text' placeholder='One Time Password' className='border-2 border-gray-600 rounded-full px-4 py-1 my-2' onChange={(e) => { setOtp(e.target.value) }}></input>
           <p className='flex justify-end'><button className='btn w-[100px] m-2' onClick={handleClick}>Submit</button></p>
           <p className='flex justify-start text-sm'>Haven't registered yet?<HashLink smooth to='/#userRegister ' className='hover:text-violet-600 mx-2'>Register Now</HashLink></p>
