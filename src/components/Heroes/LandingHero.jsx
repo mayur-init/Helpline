ee3709c402c47f8385575aa1ab78174d60c1416a
mayurdluffy <mayurcosmos007@gmail.com>
2023-05-14 15:56:01 +0530

    landing page responsive conf

--- src/components/Heroes/LandingHero.jsx
+++ src/components/Heroes/LandingHero.jsx
@@ -4,13 +4,13 @@ const LandingHero = () => {
 
   return (
     <div className='min-h-screen flex flex-col justify-center items-center bg-white' id='hero'>
-      <div className='grid md:grid-cols-2 mx-[35vh] mt-0 md:mt-[18vh]'>
-        <div className='flex flex-col justify-center md:items-start w-full mt-0 md:mt-[18vh]'>
-          <h1 className='p-2 text-5xl md:text-6xl text-gray-400 font-bold'>Book Your</h1>
-          <h1 className='px-4 text-5xl md:text-8xl font-bold hover:text-violet-600'>Services</h1>
+      <div className='grid md:grid-cols-2 w-[70vw] text-center m-8'>
+        <div className='flex flex-col justify-center md:items-start w-full mt-0 md:mt-[18vh] mb-4'>
+          <h1 className='p-2 text-4xl md:text-6xl text-gray-400 font-bold'>Book Your</h1>
+          <h1 className='px-4 text-6xl md:text-8xl font-bold hover:text-violet-600'>Services</h1>
         </div>
-        <div>
-          <img className='w-[100vh] h-[35vh] md:h-[50vh] rounded-md' src={'assets/img1.jpg'} alt="/" />
+        <div className='flex justify-center'>
+          <img className='w-[50vh] md:w-[100vh] h-[35vh] md:h-[50vh] rounded-md' src={'assets/img1.jpg'} alt="/" />
         </div>
       </div>
     </div>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,38 +7,40 @@ const Services = () => {
         <div className='w-full flex items-center' id='services'>
             <div className='w-screen m-auto'>
 
-                <h2 className='text-5xl font-bold flex justify-center my-[8vh] hover:text-violet-600'>Our Services</h2>
-                
+                <h2 className='text-5xl font-bold flex justify-center my-[6vh] hover:text-violet-600'>Our Services</h2>
+
                 {/***************card-box*************/}
-                <div className='grid md:grid-cols-3 gap-4 text-center my-[10vh] mx-8'>
-                    {/********cards*********/}
+                <div className='grid place-content-center '>
+                    <div className='grid md:grid-cols-3 gap-4 w-[75vw] mx-auto text-center md: my-[6vh]'>
+                        {/********cards*********/}
 
-                    <HashLink smooth to={'/ambulance-service#main'}>
-                    {/**********Ambulance Service************/}
-                    <div className='border h-auto bg-white m-4 p-5 rounded-xl shadow-xl hover:scale-105' >
-                        <img className='w-[80vw] h-[28vh]' src={'assets/img4.webp'} alt="/" />
-                        <p className='text-3xl font-bold m-6'>Ambulance Service</p>
-                        <p className='text-black-400 '></p>
-                    </div>
-                    </HashLink>
+                        <HashLink smooth to={'/ambulance-service#main'}>
+                            {/**********Ambulance Service************/}
+                            <div className='border md:w-[22vw] h-auto bg-white m-4 p-5 rounded-xl shadow-xl hover:scale-105' >
+                                <img className='w-[80vw] h-[28vh]' src={'assets/img4.webp'} alt="/" />
+                                <p className='text-3xl font-semibold m-7'>Ambulance Service</p>
+                                <p className='text-black-400 '></p>
+                            </div>
+                        </HashLink>
 
-                    <HashLink smooth to={'/blood-bank-service#main'}>
-                    {/******************Blood Bank Service************************/}
-                    <div className='border h-auto bg-white m-4 p-5 rounded-xl shadow-xl hover:scale-105' >
-                        <img className='w-[80vw] h-[28vh]' src={'assets/blood_bank.jpg'} alt="/" />
-                        <p className='text-3xl font-bold p-6'>Blood Bank Service</p>
-                        <p className='text-black-400 mt-2'></p>
-                    </div>
-                    </HashLink>
+                        <HashLink smooth to={'/blood-bank-service#main'}>
+                            {/******************Blood Bank Service************************/}
+                            <div className='border md:w-[22vw] h-auto bg-white m-4 p-5 rounded-xl shadow-xl hover:scale-105' >
+                                <img className='w-[80vw] h-[28vh]' src={'assets/blood_bank.jpg'} alt="/" />
+                                <p className='text-3xl font-semibold p-6'>Blood Bank Service</p>
+                                <p className='text-black-400 mt-2'></p>
+                            </div>
+                        </HashLink>
 
-                    <HashLink smooth to={'oxygen-cylinder-service#main'}>
-                    {/***********************Oxygen Cylinder Service*****************************/}
-                    <div className='border h-auto bg-white m-4 p-5 rounded-xl shadow-xl hover:scale-105' >
-                        <img className='w-full h-[28vh]' src={'assets/Oxygen_cylinder.jpg'} alt="/" />
-                        <p className='text-3xl font-bold p-6'>Oxygen Cylenders Service</p>
-                        <p className='text-black-400 mt-2'></p>
+                        <HashLink smooth to={'oxygen-cylinder-service#main'}>
+                            {/***********************Oxygen Cylinder Service*****************************/}
+                            <div className='border md:w-[22vw] h-auto bg-white m-4 p-5 rounded-xl shadow-xl hover:scale-105' >
+                                <img className='w-full h-[28vh]' src={'assets/Oxygen_cylinder.jpg'} alt="/" />
+                                <p className='text-3xl font-semibold p-6'>Oxygen Cylenders Service</p>
+                                <p className='text-black-400 mt-2'></p>
+                            </div>
+                        </HashLink>
                     </div>
-                    </HashLink>
                 </div>
             </div>
         </div>
