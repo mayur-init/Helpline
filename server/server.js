15a416e0e7994a42914cfcc8ac9ba4c83edd1cd4
mayurdluffy <mayurcosmos007@gmail.com>
2023-05-19 23:27:39 +0530

    dev conf

--- server/server.js
+++ server/server.js
@@ -6,8 +6,11 @@ const config = require('./config');
 const mongoose = require('mongoose');
 
 const app = express();
-
 app.use(cors());
+app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
 app.use(express.json());
 app.use('/api', router);
 
