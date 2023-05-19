363f1f8d639da8cf658eb2b9d25a55e8e20dced0
Mayur Rai <77693504+mayurdluffy@users.noreply.github.com>
2023-05-20 00:26:40 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(cors());
 app.use((req, res, next) =>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 })
 app.use(express.json());
