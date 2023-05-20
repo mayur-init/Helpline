d7ee7c6e312786a52cf72956c1ea6a2fb8f7700f
Ankit Burman <ank2911it@gmail.com>
2023-05-20 14:34:40 +0530

    Panel pages updated

--- src/pages/PanelPages/OxygenCylinderProviderPanel.jsx
+++ src/pages/PanelPages/OxygenCylinderProviderPanel.jsx
@@ -155,7 +155,7 @@ function OxygenCylinderProviderPanel() {
                         </div>) :
                         (<div className='h-full w-full md:w-[80vw] ml-[10vw] md:ml-[25vw]'>
                             <p className='text-2xl font-semibold text-center m-4'>Enquiries</p>
-                            <div className='bg-gray-100 w-full h-[86vh] p-4'>
+                            <div className='bg-gray-100 w-full h-full p-4'>
                                 {/****************List of related enquiries*******************/}
                                 {
                                     enquiryData !== null ? (
